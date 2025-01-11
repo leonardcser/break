2921d41b7ea4ae49d15b3c555fc439184a76b4ce
leonardcser <73912641+leonardcser@users.noreply.github.com>
2025-01-11 09:01:57 +0100

    Get app version from tauri

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -7,8 +7,15 @@ use tauri::{
 
 pub fn create(app: &AppHandle) -> tauri::Result<TrayIcon> {
     let icon = Image::from_bytes(include_bytes!("../icons/tray.ico"))?;
+    let version = app.package_info().version.to_string();
 
-    let info_i = MenuItem::with_id(app, "info", "Break v0.1.0", false, None::<&str>)?;
+    let info_i = MenuItem::with_id(
+        app,
+        "info",
+        "Break v".to_string() + &version,
+        false,
+        None::<&str>,
+    )?;
     let break_i = MenuItem::with_id(app, "break", "Break", true, None::<&str>)?;
     let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
     let menu = Menu::with_items(app, &[&info_i, &break_i, &quit_i])?;
